9b614edc5ae426582173d29e4068d02eef8aad9b
Michael Chan <mijoch@gmail.com>
2016-05-02 22:13:54 -0700

    feat(Base): media-query aware m/p props

--- example/src/example.js
+++ example/src/example.js
@@ -4,13 +4,29 @@ import {
   Base,
 } from "react-minions"
 
+const styleProps = {
+  m: {
+    mn: 0,
+    xs: .25,
+    sm: .5,
+    md: .75,
+    lg: 1,
+    xl: 2,
+  },
+  p: {
+    mn: 0,
+    xs: .25,
+    sm: .5,
+    md: .75,
+    lg: 1,
+    xl: 2,
+  },
+  unit: "rem",
+}
+
 const App = () => (
   <div>
-    <Base
-      m={0}
-      p={2}
-      unit="rem"
-    >Test</Base>
+    <Base {...styleProps}>Test</Base>
   </div>
 )
 
