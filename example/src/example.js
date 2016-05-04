d91db8666dca5816478e1260dceb7d680bc60ec3
Michael Chan <mijoch@gmail.com>
2016-05-04 13:04:14 -0700

    refactor: extract APIArticle

--- example/src/example.js
+++ example/src/example.js
@@ -1,24 +1,64 @@
-import React from "react"
+import React, { PropTypes} from "react"
 import ReactDOM from "react-dom"
 import {
   Base,
   Block,
 } from "react-minions"
 
+const APIArticle = ({
+  abstract,
+  children,
+  example,
+  title,
+  usage,
+  ...props,
+}) => (
+  <article {...props}>
+    <h2>{title}</h2>
+    <p>{abstract}</p>
+
+    {children}
+
+    <h3>Usage</h3>
+    {usage}
+
+    <h3>Example</h3>
+    <pre><code>
+      {example}
+    </code></pre>
+  </article>
+)
+APIArticle.propTypes = {
+  abstract: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  example: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
+  usage: PropTypes.node.isRequired,
+}
+APIArticle.defaultProps = {
+  children: "",
+}
+
 const App = () => (
   <div>
-    <article>
-      <h2>Block</h2>
-
-      <Block>This is a <code>Block</code></Block>
-{`<Block className="m-2" />
+    <APIArticle
+      abstract="display: block"
+      example={<Block>This is a <code>Block</code></Block>}
+      title="Block"
+      usage={`<Block className="m-2" />
 
 // => <div style="display: block" class="m-2r"></div>`}
-    </article>
+    >
+    </APIArticle>
 
-    <article>
-      <h2>Base</h2>
+    <APIArticle
+      abstract="The foundation of it all"
+      example={<Base>This is the <code>Base</code> component </Base>}
+      title="Base"
+      usage={`<Base className={{ mn: "p-0", md: "p-1" }} />
 
+// => <div class="p-0@mn p-1@md"></div>`}
+    >
       <code>Base</code> is a unique component. It can take a string or object as "className". If you provide an object, it'll honor these keys:{" "}
         <code>mn</code>,{" "}
         <code>xs</code>,{" "}
@@ -26,16 +66,7 @@ const App = () => (
         <code>md</code>,{" "}
         <code>lg</code>,{" "}
         <code>xl</code>.
-
-      <Base>This is the <code>Base</code> component </Base>
-
-      <h4>Example</h4>
-      <pre><code>
-{`<Base className={{ mn: "p-0", md: "p-1" }} />
-
-// => <div class="p-0@mn p-1@md"></div>`}
-      </code></pre>
-    </article>
+    </APIArticle>
   </div>
 )
 
