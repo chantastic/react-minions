672965ab83281fcdbaa2c37dc0936ffc142905d3
Michael Chan <mijoch@gmail.com>
2016-05-05 13:03:44 -0700

    fix: oops 'minions' -> 'minion'

--- src/minionClassNames.js
+++ src/minionClassNames.js
@@ -2,11 +2,11 @@ import classNames from "classnames"
 
 export default function (minionClassNames) {
   return classNames(
-    typeof minionsClassNames === "object" && typeof minionsClassNames.mn === "string" && minionsClassNames.mn.split(" ").map(c => `${c}@mn`).join(" "),
-    typeof minionsClassNames === "object" && typeof minionsClassNames.xs === "string" && minionsClassNames.xs.split(" ").map(c => `${c}@xs`).join(" "),
-    typeof minionsClassNames === "object" && typeof minionsClassNames.sm === "string" && minionsClassNames.sm.split(" ").map(c => `${c}@sm`).join(" "),
-    typeof minionsClassNames === "object" && typeof minionsClassNames.md === "string" && minionsClassNames.md.split(" ").map(c => `${c}@md`).join(" "),
-    typeof minionsClassNames === "object" && typeof minionsClassNames.lg === "string" && minionsClassNames.lg.split(" ").map(c => `${c}@lg`).join(" "),
-    typeof minionsClassNames === "object" && typeof minionsClassNames.xl === "string" && minionsClassNames.xl.split(" ").map(c => `${c}@xl`).join(" "),
+    typeof minionClassNames === "object" && typeof minionClassNames.mn === "string" && minionClassNames.mn.split(" ").map(c => `${c}@mn`).join(" "),
+    typeof minionClassNames === "object" && typeof minionClassNames.xs === "string" && minionClassNames.xs.split(" ").map(c => `${c}@xs`).join(" "),
+    typeof minionClassNames === "object" && typeof minionClassNames.sm === "string" && minionClassNames.sm.split(" ").map(c => `${c}@sm`).join(" "),
+    typeof minionClassNames === "object" && typeof minionClassNames.md === "string" && minionClassNames.md.split(" ").map(c => `${c}@md`).join(" "),
+    typeof minionClassNames === "object" && typeof minionClassNames.lg === "string" && minionClassNames.lg.split(" ").map(c => `${c}@lg`).join(" "),
+    typeof minionClassNames === "object" && typeof minionClassNames.xl === "string" && minionClassNames.xl.split(" ").map(c => `${c}@xl`).join(" "),
   )
 }
